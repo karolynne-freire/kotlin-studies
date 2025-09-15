d88a15d0a7d40aa4e2e8fc988e538d5babd5eafe
karolynne-freire <freirekarolynne@gmail.com>
2025-09-14 23:39:30 -0300

    feat: primeiro projeto em kotlin

---
+++ projetos/primeiroapp/app/src/main/java/com/example/primeiroapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.primeiroapp
+
+import android.app.ActivityManager
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.primeiroapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOla.setOnClickListener{
+            val nome = binding.editText.text.toString()
+            binding.textViews.setText("olá ${nome}")
+        }
+    }
+}
